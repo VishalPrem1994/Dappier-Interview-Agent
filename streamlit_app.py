03dec71ce555022f1cebea9f1a83ff1d8dcab862
Vishal Prem <vishalpremdass@gmail.com>
2024-09-26 20:22:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,12 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain.chains import create_retrieval_chain
 
-
 import os
 
 # Show title and description.
 st.title("💬 Interview Assistant")
 st.write(
-    "This is a simple Interviewer that asks questions based on the Job Description Provided. \nAfter 4 Questions it will evaluate the candidate based on the responses."
+    "This is a simple Interviewer that asks questions based on the Job Description. Upload the job description file and answer the questions provied. After 4 Questions it will evaluate the candidate based on the responses."
 )
 
 openai_api_key = st.text_input("OpenAI API Key", type="password")
@@ -33,7 +32,6 @@ if openai_api_key:
 
     if (uploaded_file):
 
-
         article = uploaded_file.read().decode()
 
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
@@ -42,11 +40,13 @@ if openai_api_key:
         vectorstore = FAISS.from_texts(texts=splits, embedding=OpenAIEmbeddings())
         retriever = vectorstore.as_retriever(search_kwargs={"k": 6})
 
-        llm = ChatOpenAI(model="gpt-4o", openai_api_key=openai_api_key)
+        questionaire_llm = ChatOpenAI(model="gpt-4o", openai_api_key=openai_api_key)
         evaluation_llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key=openai_api_key)
         # Define prompt template
         template = """
-        You are an interview assistant for generating a single question based on a resume. You will be provided with all previous questions asked.
+        You are an interview assistant for generating a single question based on a job description.
+        Make sure to ask questions about the skills and experience level required in the job description. 
+        You will be provided with all previous questions asked.
         Use the provided context only to generate questions:
 
         <context>
@@ -54,10 +54,7 @@ if openai_api_key:
         </context>
 
         Previous Conversations: {input}
-
-
         Generate just one question that is not part of the previous conversation and nothing else.
-
         """
 
         st.button('Clear Chat', on_click=click_button)
@@ -71,7 +68,7 @@ if openai_api_key:
             st.chat_message(msg["role"]).write(msg["content"])
         prompt = ChatPromptTemplate.from_template(template)
 
-        doc_chain = create_stuff_documents_chain(llm, prompt)
+        doc_chain = create_stuff_documents_chain(questionaire_llm, prompt)
         chain = create_retrieval_chain(retriever, doc_chain)
 
         if prompt := st.chat_input():
@@ -80,9 +77,12 @@ if openai_api_key:
                 st.chat_message("user").write(prompt)
                 evaluation_template = """Evaluate the following responses of a candidate regarding a job.
                                 """ + str(st.session_state.messages) + """ 
-                               Respond with feedback on the candidate. The candidate should be able to give detailed responses to the questions asked to get a good score. 
+                                Give a score out of 10 based on the candidate's experience with the skills required in the job description.
+                                The candidate should be able to give detailed responses to the questions asked to get a good score above 6.
+                               Also respond with feedback on the candidate.  
                                Be strict in your evaluation.
-                               Give a score out of 10 based on the candidate's experience with the skills required in the job description."""
+                            
+                            """
                 response = evaluation_llm(evaluation_template)
                 st.session_state.messages.append({"role": "assistant", "content": response})
                 st.chat_message("assistant").write(response)
@@ -97,5 +97,3 @@ if openai_api_key:
 
                 st.session_state.messages.append({"role": "assistant", "content": msg})
                 st.chat_message("assistant").write(msg)
-
-
